d33e0c04b2b888f20b12fa3a738bdbcc795eec29
GuyEdit <69353199+GuyEditDev@users.noreply.github.com>
2021-07-17 13:42:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,2 @@
 import os
-import platform
-if platform.system() == "Windows":
-	os.system("pip install colorama")
-elif platform.system() == "Linux":
-	os.system("pip install colorama")
+os.system("pip install colorama")
